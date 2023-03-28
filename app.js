01e4bcefc80f4cab32f82f5807ba7df2dfb41c1b
dandubitsky <dan.dubitsky@gmail.com>
2023-03-27 22:02:10 -0700

    Lesson 4 loops
    test
    commit
    starting

---
+++ app.js
@@ -0,0 +1,57 @@
+// for(let i=0; i<5; i++){
+//     console.log("Hello", i);
+// }
+
+// for(let i = 10; i>5; i=i-1){
+//     console.log("bye");
+// }
+
+// for (let j=10; j<=11; j=j+1){
+//     console.log("say")
+// }
+
+ //const array = ["one", "two", 3, "four", 5.0];
+
+// for (let i = 0; i<5; i++){
+//     console.log("i", i)
+//     console.log(array[i])
+// }
+
+// for ( let i=4; i>=0; i--){
+//     console.log("i",i)
+//     console.log(array[i])
+// }
+
+// for (let v of array){
+//     console.log(v);
+// }
+
+// const fruits = ["Apple", "Peach", "Plum", "Pear", "Kiwi", "Orange"];
+
+// for(let fruit of fruits){
+//     console.log(fruit);
+// }
+
+const arr = [1,2,5,7,9,10,3,7];
+
+let index = arr.indexOf
+// let sum = 0;
+// for (let v of arr){
+//     sum = sum+v;
+// }
+// console.log(sum);
+// console.log(arr.length);
+// for (let i of arr){
+//     console.log(i);
+// }
+
+// for (let i=arr.length-1; i>=0; i--){
+//     console.log(arr[i]);
+// }
+// const reversedArray = [];
+// for (let i=arr.length-1; i>=0; i--){
+//     const v = arr[i];
+//     reversedArray.push(v);
+// }
+// console.log(reversedArray);
+
